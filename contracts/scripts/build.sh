12dc4af4aba232c7e00c7013780d20541e57e990
Fabian Piper <fabianpiper@web.de>
2021-11-16 13:57:12 +0100

    Remove unused sh

--- contracts/scripts/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Use this as an alternative to docker
-
-ligo compile-contract contracts/counter.ligo main > build/counter.tz
--- contracts/scripts/deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Use this as an alternative to deploy.js (change values)
-
-tezos-client originate contract <contract_name> for <user> transferring <amount_tez> from <from_user> running <tz_file> --init '<initial_storage>' --burn-cap <gaz_fee>

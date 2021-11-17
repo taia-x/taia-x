3a2aa41a5095e5eeddb002aa63cd0811edd02623
alvaro-alonso <alv@Alvs-MBP.fritz.box>
2021-11-17 19:02:17 +0100

    #24 add container debugging

--- backend/docker-start.sh
+++ backend/docker-start.sh
@@ -3,4 +3,9 @@
 pip install --upgrade pip
 pip install -r requirements.txt
 
-uvicorn app.main:app --reload --host 0.0.0.0
+if [[ "$DEBUG" = true ]]; then
+    pip install debugpy
+    python -m debugpy --wait-for-client --listen 0.0.0.0:5678 -m uvicorn app.main:app --reload --host 0.0.0.0
+else
+    uvicorn app.main:app --reload --host 0.0.0.0
+fi

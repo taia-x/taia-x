5795448e88538c653aaa4a60c7b2c2bea964eebd
alvaro-alonso <alv@Alvs-MBP.fritz.box>
2021-11-18 23:18:10 +0100

    #24 DigitalTwinInterface validation rules

--- backend/app/routers/ontologies.py
+++ backend/app/routers/ontologies.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 
 from ..db import get_db
 from ..core.ipfs import ipfs_client
-from ..schemas import Ontology
+from ..schemas import DigitalTwinInterface
 from ..models import get_or_create_ontology
 
 
@@ -14,9 +14,9 @@ router = APIRouter(
 
 
 @router.post("/")
-async def save_ontology(ontology: Ontology, db: Session = Depends(get_db)):
+async def save_ontology(ontology: DigitalTwinInterface, db: Session = Depends(get_db)):
     ontology_hash = ipfs_client.save_json(ontology.dict())
-    db_ontology = get_or_create_ontology(db, ontology_hash)
+    db_ontology, _ = get_or_create_ontology(db, ontology_hash)
     return {
         "ontology_hash": db_ontology.ontology_addr,
         "ontology": ontology

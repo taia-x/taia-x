8742afa07e5259730f32edc93630efdf579805f8
Kevin Hertwig <hertwig@campus.tu-berlin.de>
2021-11-20 04:48:00 +0100

    #24 added frontend to docker-compose with hot-reload

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -6,16 +6,33 @@ module.exports = {
     setup(app) {
       app.use("/__open-in-editor", openInEditor("code"));
     },
+    host: "0.0.0.0", // to accept connections from outside container
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "*",
+    },
+    proxy: {
+      "/api": {
+        target: "http://taia-x-backend:8000",
+      },
+    },
+    watchOptions: {
+      aggregateTimeout: 500, // delay before reloading
+      poll: 1000, // enable polling since fsevents are not supported in docker
+    },
   },
   configureWebpack: {
     performance: {
       hints: false,
     },
     optimization: {
-      splitChunks: {
-        minSize: 10000,
-        maxSize: 250000,
-      },
+      splitChunks:
+        process.env.NODE_ENV === "production"
+          ? {
+              minSize: 10000,
+              maxSize: 250000,
+            }
+          : undefined,
     },
   },
 };

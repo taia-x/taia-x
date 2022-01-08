bd68c633b59dcb1d2c61c8440fef0c42a4d1196f
AndrelarsHub <andremecklenborg@hotmail.com>
2022-01-08 15:42:11 +0100

    Adding testing for sig-verification using taquito

---
+++ contracts/sig-verification/testing/verify_sig.ts
@@ -0,0 +1,37 @@
+import { TezosToolkit } from '@taquito/taquito';
+import { InMemorySigner } from '@taquito/signer';
+
+
+async function verify_sig() {
+    const provider = 'https://hangzhounet.api.tez.ie';
+    const signer: any = new InMemorySigner('edsk3JRacRgUiZY7ciHsqRbPEqBcHASQpvoHoUAvmB1uq7Fu5RHzam');
+        const tezos = new TezosToolkit(provider);
+    tezos.setSignerProvider( signer );
+    try {
+        console.log("signer pkh:");
+        console.log(await signer.publicKeyHash());
+        const contract = await tezos.contract.at('KT1QS8m9TzQF3PvJzxpDgqHYGTtBWqW3FCev');
+        console.log("Printing contract methods...");
+        console.log(contract.methods);
+        console.log("Showing initial storage...");
+        console.log(await contract.storage())
+        console.log(`Interacting with Smart Contract..`);
+        console.log(`First show the signature of the method`);
+        console.log(contract.methodsObject.verifySig1().getSignature())
+        console.log(`Now send a test transaction..`);
+        contract.methodsObject.verifySig1({
+            message_packed: '6d6279746573',
+            pbk: 'edpkuYzXL37pTYVHu7rTodeyH9bQ2rfDKadWqqGCqL8m8G1W4UH55E',
+            provider_sig:  'edsigtffaz6wutLRzi7Ni15KPcoUZULJiyF3tHX78iBp17Fdt4jTTDmJjVe6QfK8ggimYGDcv7bLFziMPKSP2JxG5sb8NGKZcQG'
+        }).send().then((op) => {
+            console.log(`Awaiting for ${op.hash} to be confirmed...`);
+             return op.confirmation().then(() => op.hash);
+          }).then((hash) => console.log(`Operation injected: https://hangzhou.tzstats.com/${hash}`))
+          .catch((error) => console.log(`Error: ${JSON.stringify(error, null, 2)}`))
+    } catch (ex) {
+        console.log(ex)
+    }
+}
+
+// tslint:disable-next-line: no-floating-promises
+verify_sig();

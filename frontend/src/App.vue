4725eaa1c8765e5cb6da256a009b4d2b7c1d0df3
Kevin Hertwig <hertwig@campus.tu-berlin.de>
2022-01-24 01:13:02 +0100

    added on_update_role handler to dipdup to get current role for authenticated user

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -11,11 +11,13 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, onMounted } from "vue";
+import { defineComponent, onMounted } from "vue";
 import Header from "@/components/Header/Header.vue";
 import AlertWrapper from "@/components/Utils/AlertWrapper.vue";
 import { useUserStore } from "@/stores/useUser";
 import initInterfaces from "@/services";
+import { storeToRefs } from "pinia";
+import { HTTP_METADATA_API_URL } from "@/constants";
 
 export default defineComponent({
   components: {
@@ -25,11 +27,37 @@ export default defineComponent({
   setup() {
     const user = useUserStore();
     const { initializeUser } = user;
+    const { address } = storeToRefs(user);
+
+    const setRole = async () => {
+      const result = await fetch(HTTP_METADATA_API_URL, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          query: `
+              query GetAccount($address: String = "") {
+                account_by_pk(address: $address) {
+                  address
+                  role
+                }
+              }
+            `,
+          variables: {
+            address: address.value,
+          },
+        }),
+      });
+      const { data } = await result.json();
+      user.$patch((state) => (state.role = data.account_by_pk.role));
+    };
 
     onMounted(async () => {
       try {
         await initInterfaces();
         await initializeUser();
+        if (address.value) setRole();
       } catch (e: any) {
         throw new Error(e.toString());
       }

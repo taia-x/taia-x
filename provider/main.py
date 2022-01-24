57dc8c4e7b6d5ea2fb41a8ad10601da83f4d277c
Kaustabh Barman <kaustabhbarman@kaustabhs-mbp.localdomain>
2022-01-24 23:25:19 +0100

    #115 download endpoint functional

--- provider/main.py
+++ provider/main.py
@@ -1,14 +1,10 @@
-from typing import Optional
-
 from fastapi import FastAPI, Depends, File, UploadFile
+from fastapi.responses import FileResponse
 from functools import lru_cache
 import config
 from uuid import uuid4
 from pathlib import Path
-# from sqlalchemy.orm import Session
-# from schemas import CreateNftdataRequest
-# from database import get_db
-# from models import Nftdata
+import glob
 
 app = FastAPI()
 
@@ -16,22 +12,29 @@ app = FastAPI()
 def get_settings():
     return config.Settings()
 
-#TAIA-X uses this API to fetch the real
+#TAIA-X uses this API to fetch the data
 @app.post("/download/{unique_id}")
-def fetch_data(nft_id: str, sig: str, pbkey: str):
+def fetch_data(unique_id: str, nft_id: str, sig: str, pbkey: str):
     #TODO ownership verification needs to be implemented here, also sig as an argument
-    data_path = f'assets/{unique_id}'
+    file = glob.glob(f'assets/{unique_id}/*.zip')[0]
+    return FileResponse(file)
 
-# Providers API to upload data and mint NFT -> multiple files
-#TODO Check file format if it's zip or not and respond accordingly
+
+# Providers API to upload data and mint NFT -> multiple files not handled
 @app.post("/upload/")
 async def upload(datafile: UploadFile = File(...), settings: config.Settings = Depends(get_settings)):
-    uid = str(uuid4())
-    store_path = f"assets/{uid}/"
-    Path(store_path).mkdir(parents=True, exist_ok=True)
-    data_path = store_path + datafile.filename
-    with open(data_path, "wb+") as file_object:
-        file_object.write(datafile.file.read())
-    return {
-        "success": f"{settings.app_server}/download/{uid}" #this will be visible only once
-    }
+    supported_formats = ['zip', 'rar']
+    if datafile.content_type.split("/")[1] in supported_formats:
+        uid = str(uuid4())
+        store_path = f"assets/{uid}/"
+        Path(store_path).mkdir(parents=True, exist_ok=True)
+        data_path = store_path + datafile.filename
+        with open(data_path, "wb+") as file_object:
+            file_object.write(datafile.file.read())
+        return {
+            "success": f"{settings.app_server}/download/{uid}" #this will be visible only once
+        }
+    else:
+        return {
+            "error": "Please upload either zip or rar file"
+        }

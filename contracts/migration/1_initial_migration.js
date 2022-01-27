651ef851a848f79bafe09e257eba6cff056693ac
Kevin Hertwig <hertwig@campus.tu-berlin.de>
2022-01-27 13:22:19 +0100

    set jon as certifier

--- contracts/migration/1_initial_migration.js
+++ contracts/migration/1_initial_migration.js
@@ -20,17 +20,16 @@ module.exports = async (Tezos) => {
   const token_metadata = new MichelsonMap();
   const empty_ledger = new MichelsonMap();
   const empty_operators = new MichelsonMap();
-  const users = new MichelsonMap()
+  const users = new MichelsonMap();
 
   // Set deployment account address as the only certifier
   users.set(
     {
-      0: await Tezos.wallet.pkh(), // addresss
-      1: { "certifier": {} }, // role
+      0: "tz1ggvpTMyxX5QVYqbpLVmNGCsgDpDyUMawq", // addresss of jon
+      1: { certifier: {} }, // role
     },
-    [['unit']]
+    [["unit"]]
   );
-  
 
   return {
     market: market,

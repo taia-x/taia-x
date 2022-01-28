b64d51defcd9e92673ee2009373f27c0896fee87
AndrelarsHub <andremecklenborg@hotmail.com>
2022-01-28 17:54:29 +0100

    POST request of dataset download

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,5 @@
 import uvicorn
+import pysodium
 from datetime import datetime
 from fastapi import FastAPI, Depends, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
@@ -6,6 +7,9 @@ from fastapi.responses import FileResponse
 from pydantic import BaseModel
 from uuid import uuid4
 from pathlib import Path
+from pyblake2 import blake2b
+from encoding import scrub_input, base58_decode, base58_encode
+
 import glob
 import socket
 
@@ -22,18 +26,61 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 class AuthData(BaseModel):
     nft_id: str
     sig: str
     pbkey: str
 
+
+def public_key_hash(public_point) -> str:
+    """Creates base58 encoded public key hash for this key.
+
+    :returns: the public key hash for this key
+    """
+    pkh = blake2b(data=public_point, digest_size=20).digest()
+    prefix = b'tz1'
+    return base58_encode(pkh, prefix).decode()
+
+
 # get digital twin data by unique id
 @app.post("/download/{unique_id}")
-def fetch_data(data: AuthData, unique_id):
-    #TODO hash pbkey and check if it matches to account address that purchased nft_id 
-    #TODO verify signature to check authenticity of requester
-    file = glob.glob(f'assets/{unique_id}/*.zip')[0]
-    return FileResponse(file)
+async def fetch_data(unique_id: int, data: AuthData):
+    results = {"unique_id": unique_id}
+    # results = {"unique_id": unique_id, "nft_id": data.nft_id, "sig": data.sig, "public key": data.pbkey}
+    results.update({"data": data})
+    print(results)
+
+    # hash pbkey and check if it matches to account address that purchased nft_id
+    pbk = scrub_input(data.pbkey)
+    pbk2 = base58_decode(pbk)
+    pkh = blake2b(data=pbk2, digest_size=20).digest()
+    prefix = b'tz1'
+    pbh2 = base58_encode(pkh, prefix).decode()
+    pbh_of_buyer = ""  # to be called from database
+    print("Check matching onchain PBK from buyer with consumer pbk..")
+    if pbh2 == pbh_of_buyer:
+        print("Verified account!")
+    else:
+        print("Not the same account!")
+        # Change to return, but for test purposes, pass
+        pass
+
+    # verify signature to check authenticity of requester
+    signature = scrub_input(data.sig)
+    message = scrub_input(data.nft_id)
+
+    signature = base58_decode(signature)
+
+    digest = pysodium.crypto_generichash(message)
+    try:
+        pysodium.crypto_sign_verify_detached(signature, digest, pbk2)
+    except ValueError:
+        raise ValueError('Signature is invalid.')
+    print("Signature is valid!")
+
+    # file = glob.glob(f'assets/{unique_id}/*.zip')[0]
+    return None  # return file
 
 
 # upload new digital twin data and save on file system
@@ -48,16 +95,19 @@ async def upload(archive: UploadFile = File(...)):
         with open(data_path, "wb+") as zip_file:
             zip_file.write(archive.file.read())
         return {
-            "artifactUri": f"http://localhost:8000/download/{uid}" # returns artifactUri to be stored in token metadatafile
+            "artifactUri": f"http://localhost:8000/download/{uid}"
+            # returns artifactUri to be stored in token metadatafile
         }
     else:
         return {
             "error": "Unsupported file format! Please upload either zip or rar file."
         }
 
+
 @app.get("/health")
 async def health():
     return "healthy"
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
--- backend/app/test_main.py
+++ backend/app/test_main.py
@@ -8,3 +8,16 @@ client = TestClient(app)
 def test_read_health():
     response = client.get("/health")
     assert response.status_code == 200
+
+
+def test_fetch_data():
+    json_test = {
+        "nft_id": "tk1",
+        "sig": "edsigtwdx5PQq25GaBZj895dx5ZvKmzh1vJncsHQJgGfMz9Z5PM6GpXpKXDLnJLfdcbGHRY93CaTbvcDk3Mb7CJpusoUkmbNtAD",
+        "pbkey": "edpkugjv7zR7CNiEZH8f1otn49iCzgRKeDMhuimA7djN4NQFvU76NM"
+    }
+    response = client.post(url="/download/1", json=json_test)
+    print(response)
+
+
+test_fetch_data()

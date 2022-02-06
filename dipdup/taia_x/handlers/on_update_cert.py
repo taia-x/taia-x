037f401387695c8ee63722ffb5fb6f98a04ca116
Kevin Hertwig <hertwig@campus.tu-berlin.de>
2022-02-06 01:35:01 +0100

    added cert event for dipdup

--- dipdup/taia_x/handlers/on_update_cert.py
+++ dipdup/taia_x/handlers/on_update_cert.py
@@ -17,3 +17,21 @@ async def on_update_cert(
     token, _ = await models.Token.get_or_create(id=nft_id)
     token.cert_state = models.CertState.certified if list(json.loads(update_certs.parameter.json())[0].keys())[0] == "certify" else models.CertState.rejected
     await token.save()
+
+    # save or get account that certified/rejected the token
+    caller, _ = await models.Account.get_or_create(address=update_certs.data.sender_address)
+
+    # get contract that received entrypoint call
+    recipient, _ = await models.Account.get_or_create(address=update_certs.data.target_address)
+
+    # save cert event
+    cert_event = models.Event(
+        token=token,
+        _from=caller,
+        _to=recipient,
+        event_type=models.EventType.certify if token.cert_state == models.CertState.certified else models.EventType.reject,
+        ophash=update_certs.data.hash,
+        level=update_certs.data.level,
+        timestamp=update_certs.data.timestamp,
+    )
+    await cert_event.save()

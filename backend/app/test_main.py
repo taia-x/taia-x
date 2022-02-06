1fbeef488ff1105b4168cd7209823e3faecc8ffb
Kevin Hertwig <hertwig@campus.tu-berlin.de>
2022-02-06 02:07:12 +0100

    fix backend test

--- backend/app/test_main.py
+++ backend/app/test_main.py
@@ -12,9 +12,10 @@ def test_read_health():
 
 def test_fetch_data():
     json_test = {
-        "nft_id": "tk1",
-        "sig": "edsigtwdx5PQq25GaBZj895dx5ZvKmzh1vJncsHQJgGfMz9Z5PM6GpXpKXDLnJLfdcbGHRY93CaTbvcDk3Mb7CJpusoUkmbNtAD",
-        "pbkey": "edpkugjv7zR7CNiEZH8f1otn49iCzgRKeDMhuimA7djN4NQFvU76NM"
+        "nft_id": "2",
+        "sig": "edsigu4f3h8i2vp29D91c3HJQgjsyNRieYUY1AsVw7Cx1n1N7Dn6YcCRFJ9mktXoTyDmLKojdnLW4UgXb5V5U9xDbM26fEtkvaG",
+        "pbkey": "edpkv2PkEkoaYN9KP769GrFgshMoVn8cvHUuYVUkogxiqZMctxPbB8",
+        "message": "050132"
     }
     response = client.post(url="/download/1", json=json_test)
     print(response)
